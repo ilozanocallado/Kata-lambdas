c2a660d54fd0ec5ede9a363e2702f8fc57ffdd64
ilozano <ilozanocallado@gmail.com>
2023-05-15 16:08:39 +0200

    Initial Version

---
+++ src/main/java/kata/lambdas/part1/FunctionalProgramming.java
@@ -0,0 +1,8 @@
+package kata.lambdas.part1;
+
+import java.util.function.Function;
+
+public class FunctionalProgramming {
+    public static Function<?, ?> f = ;//Make me a function; remember to set the types!
+
+}
---
+++ src/main/java/kata/lambdas/part2/FunctionalProgramming.java
@@ -0,0 +1,9 @@
+package kata.lambdas.part2;
+
+import java.util.function.WhatFunction?;
+
+public class FunctionalProgramming {
+    public static WhatFunction? f = ;//Make me a function.
+    //Don't use Function. Find the appropriate function to turn an
+    //arbitrary class into a double. Remember the import.
+}
